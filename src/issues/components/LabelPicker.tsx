0f3b909ce5be3fc0fe19d89965b633aee98e65b8
cob3x85 <cob3x85@outlook.com>
2023-05-07 21:52:46 -0500

    Created axio request to get labels and created the Labels interface

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,14 +1,33 @@
-
+import { useQuery } from "@tanstack/react-query";
+import { githubApi } from "../../api/githubApi";
+import { Label } from "../interfaces/label";
 export const LabelPicker = () => {
+
+  const getLabels = async(): Promise<Label[]> => {
+    const { data } = await githubApi.get<Label[]>('/labels');
+    // const res = await fetch('https://api.github.com/repos/facebook/react/labels');
+    // const data = await res.json();
+    console.log(data);
+    return data;
+  }
+
+  const labelsQuery = useQuery(
+    ['labels'],
+    getLabels,
+    {
+      refetchOnWindowFocus: false
+    }
+  );
+
   return (
     <div>
-        <span 
+        <span
             className="badge rounded-pill m-1 label-picker"
             style={{ border: `1px solid #ffccd3`, color: '#ffccd3' }}
         >
             Primary
         </span>
-        
+
     </div>
   )
 }
